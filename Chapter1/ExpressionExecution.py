08d8e782aa34e5e3f33510e57f0ab74aa9124892
Daniyal <daniyalarain123786@gmail.com>
2025-12-29 02:51:41 +0500

    Create ExpressionExecution.py for arithmetic examples
    Add a new Python script for expression execution demonstrating various arithmetic operations.

---
+++ Chapter1/ExpressionExecution.py
@@ -0,0 +1,20 @@
+#EXPRESSION EXECUTION
+a,b,c = 5,3,2.0
+text,text2 = "@","$"
+print(a*text)
+print((text+text2)*b)
+
+print(a/b)
+print(a/c)
+
+#INTEGER DIVISION
+print(a//c)
+print(12//5)
+print(-12//5)
+print(12//-5)
+
+#MODULUS
+print(5 % 2)
+print(-5 % -2)
+print(5 % -2)
+print(-5 % 2)
