3570b6a55e702f0eb096cdd891ce3b4c5ebf437a
Daniyal <daniyalarain123786@gmail.com>
2025-12-29 03:08:40 +0500

    Add nesting problem for age-based driving eligibility

---
+++ Chapter2/Conditional_Statements/Nesting_Problem.py
@@ -0,0 +1,10 @@
+#NESTING
+
+age = int(input("ENTER YOUR AGE : "))
+if(age>=18):
+    if(age>=80):
+        print("YOU CAN NOT DRIVE")
+    else:
+        print("YOU CAN DRIVE")
+else:
+    print("YOU CAN NOT DRIVE")
