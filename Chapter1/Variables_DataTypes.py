5a3ab508edbb4adc5d5f0822b92a3eb7eb93ae0c
Daniyal <daniyalarain123786@gmail.com>
2025-12-29 02:49:42 +0500

    Add Variables_DataTypes.py with examples
    Introduced a Python script demonstrating variables and data types.

---
+++ Chapter1/Variables_DataTypes.py
@@ -0,0 +1,22 @@
+#VARIABLES
+name = "DANIYAL"
+age = 19
+salary = 56.25
+c = True
+value = None
+print("My name is :",name)
+print("My age is :",age)
+print("My salary is :",salary)
+
+#DATA TYPES
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(c))
+print(type(value))
+
+a = 10
+b = 5
+# a , b = 10 , 5
+sum = a + b
+print("SUM =",sum)
