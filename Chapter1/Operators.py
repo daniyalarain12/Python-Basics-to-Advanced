5b3738e0c546895770c7cccc920ad68469800983
Daniyal <daniyalarain123786@gmail.com>
2025-12-29 02:55:26 +0500

    Add Operators.py with examples of various operators

---
+++ Chapter1/Operators.py
@@ -0,0 +1,62 @@
+#OPERATORS
+
+#1. ARITHMETIC OPERATORS
+# a = 5
+# b = 2
+# print("SUM :", a + b)
+# print("DIFFERENCE :", a - b)
+# print("PRODUCT :", a * b)
+# print("DIVISION :", a / b)
+# print("MODULUS/REMAINDER :", a % b)
+# print("a POWER b :", a ** b)
+
+#2. RELATIONAL/COMPARISON OPERATOR
+# a = 50
+# b = 20
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a >= b)
+# print(a < b)
+# print(a <= b)
+
+#3. ASSIGNMENT OPERATORS
+# num = 10
+# num += 3
+# print("NUMBER :",num)
+
+# num = 10
+# num -= 3
+# print("NUMBER :",num)
+
+# num = 10
+# num *= 3
+# print("NUMBER :",num)
+
+# num = 10
+# num /= 2
+# print("NUMBER :",num)
+
+# num = 10
+# num %= 3
+# print("NUMBER :",num)
+
+# num = 10
+# num **= 3
+# print("NUMBER :",num)
+
+#4. LOGICAL OPERATORS
+
+# a = 50
+# b = 30
+# val1 = True
+# val2 = False
+
+# print("RESULT OF and OPERATOR :", val1 and val2)
+# print("RESULT OF or OPERATOR :", val1 or val2)
+
+# print("RESULT OF and OPERATOR :", (a==b) and (a>b))
+# print("RESULT OF or OPERATOR :", (a==b) or (a>b))
+
+# print(not False)
+# print(not (a>b))
