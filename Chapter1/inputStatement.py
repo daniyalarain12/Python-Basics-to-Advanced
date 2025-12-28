b47f07da3cd97d7f17ddb4c406917e78f89f9338
Daniyal <daniyalarain123786@gmail.com>
2025-12-29 02:53:15 +0500

    Add input statements for name, age, and height

---
+++ Chapter1/inputStatement.py
@@ -0,0 +1,7 @@
+#INPUTS
+name = input("ENTER YOUR NAME : ")
+age = int(input("ENTER YOUR AGE : "))
+height = float(input("ENTER YOUR HEIGHT : "))
+print("NAME :",name)
+print("AGE :",age)
+print("HEIGHT :",height)
