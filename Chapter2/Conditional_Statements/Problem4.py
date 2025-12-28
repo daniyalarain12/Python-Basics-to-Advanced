039bc2567ace70d1badfb3783d97265a0272c84e
Daniyal <daniyalarain123786@gmail.com>
2025-12-29 03:06:15 +0500

    Add program to check if a number is odd or even

---
+++ Chapter2/Conditional_Statements/Problem4.py
@@ -0,0 +1,7 @@
+#WRITE A PROGRAM TO CHECK IF A NUMBER ENTERED BY THE USER IS ODD OR EVEN.
+
+num = int(input("ENTER ANY NUMBER : "))
+if(num%2 == 0):
+    print("NUMBER IS EVEN")
+else:
+    print("NUMBER IS ODD")
