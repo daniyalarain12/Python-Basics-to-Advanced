fe846509ded12c764099c1373c7d83069ca7ef9e
Daniyal <daniyalarain123786@gmail.com>
2025-12-29 03:00:21 +0500

    Add program to calculate average of two numbers
    This program calculates the average of two floating-point numbers input by the user.

---
+++ Chapter1/Problem4.py
@@ -0,0 +1,6 @@
+#WRITE A PROGRAM TO INPUT 2 FLOATING NUMBERS AND PRINT THEIR AVERAGE.
+
+num1 = float(input("ENTER 1ST NUMBER : "))
+num2 = float(input("ENTER 2ND NUMBER : "))
+avg = (num1 + num2) /2
+print("AVERAGE :",avg)
