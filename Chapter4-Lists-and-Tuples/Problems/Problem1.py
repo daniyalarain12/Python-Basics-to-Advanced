56e7cdf3f017c917333fa402fc522ac188578e49
Daniyal <daniyalarain123786@gmail.com>
2025-12-30 02:41:51 +0500

    Add program to store favorite movies in a list

---
+++ Chapter4-Lists-and-Tuples/Problems/Problem1.py
@@ -0,0 +1,8 @@
+#WRITE A PROGRAM TO ASK THE USER TO ENTER NAMES OF THEIR 3 FAVORITE MOVIES AND STORE THEM IN A LIST.
+
+movies = []
+print("ENTER YOUR 3 FAVORITE MOVIES :")
+movies.append(input("ENTER MOVIE 1 : "))
+movies.append(input("ENTER MOVIE 2 : "))
+movies.append(input("ENTER MOVIE 3 : "))
+print("YOUR 3 FAVORITE MOVIES ARE :", movies)
