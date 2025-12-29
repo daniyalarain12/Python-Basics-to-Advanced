d4f4eb18370ebf22948609421a41fcc744db452c
Daniyal <daniyalarain123786@gmail.com>
2025-12-30 00:56:14 +0500

    Add basic list operations in lists-basics.py
    Added basic examples of lists in Python, including creation, printing, and accessing elements.

---
+++ Chapter4-Lists-and-Tuples/Lists/lists-basics.py
@@ -0,0 +1,17 @@
+#LISTS IN PYTHON
+#LIST IS A BUILT-IN DATA TYPE THAT CAN STORE DIFFERENT TYPE OF DATA.
+
+marks = [98.3,67.5,90.5,78.9,79.6]
+print(marks)
+print(type(marks))
+print("LENGTH :", len(marks))
+print(marks[0])
+print(marks[1])
+print(marks[2])
+print(marks[3])
+print(marks[4])
+
+student = ["DANIYAL", 19, 5.11, "HYDERABAD"]
+print(student)
+student[2] = 5.9
+print(student)
