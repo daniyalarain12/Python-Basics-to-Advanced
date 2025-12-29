9bb6503b8b1da6d72ccacf89fb30c88542c4e18e
Daniyal <daniyalarain123786@gmail.com>
2025-12-30 02:47:57 +0500

    Add program to count 'A' grades in a tuple
    This program counts the number of students with the 'A' grade in a tuple of grades.

---
+++ Chapter4-Lists-and-Tuples/Problems/Problem3.py
@@ -0,0 +1,4 @@
+#WRITE A PROGRAM TO COUNT THE NO. OF STUDENTS WITH THE "A" GRADE IN THE FOLLOWING TUPLE. ["C","D","A","A","B","B","A"]
+
+grade = ("C","D","A","A","B","B","A")
+print("COUNT :", grade.count("A"))
