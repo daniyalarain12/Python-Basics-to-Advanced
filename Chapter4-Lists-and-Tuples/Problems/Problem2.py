114b41be4581268f013b1272cf50b507ef2cae65
Daniyal <daniyalarain123786@gmail.com>
2025-12-30 02:42:50 +0500

    Add palindrome check for list elements
    Implement palindrome check for a list.

---
+++ Chapter4-Lists-and-Tuples/Problems/Problem2.py
@@ -0,0 +1,9 @@
+#WRITE A PROGRAM TO CHECK IF A LIST CONTAINS A PALINDROME OF ELEMENTS.
+
+list = [1, 2, 1]
+copy_list = list.copy()
+copy_list.reverse()
+if(list == copy_list):
+    print("LIST CONTAINS A PALINDROME OF ELEMENTS")
+else:
+    print("LIST DOES NOT CONTAINS A PALINDROME OF ELEMENTS")
