8319ee1f1c3595a25093d431eafa4b8a9993bf08
Daniyal <daniyalarain123786@gmail.com>
2025-12-30 01:58:45 +0500

    Add basic tuple examples in Tuples-Basics.py
    Added examples demonstrating tuple creation and types.

---
+++ Chapter4-Lists-and-Tuples/Tuples/Tuples-Basics.py
@@ -0,0 +1,19 @@
+#TUPLES IN PYTHON
+#TUPLE IS A BUILT-IN DATA TYPE AND IS IMMUTABLE IN NATURE.
+
+tup = (1,2,3,4)
+print(type(tup))
+print(tup)
+print(tup[0])
+print(tup[1])
+print(tup[2])
+print(tup[3])
+
+#TYPES OF TUPLES
+
+tup1 = ()
+tup2 = (1,)
+tup3 = (1,2,3,4)
+print(tup1)
+print(tup2)
+print(tup3)
