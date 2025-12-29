d4706525750f2dab236d339d0e9021efb87e5d85
Daniyal <daniyalarain123786@gmail.com>
2025-12-29 22:28:42 +0500

    Implement string indexing and slicing examples
    Add examples of indexing, slicing, and negative indexing in strings.

---
+++ Chapter3-Strings/Indexing_And_Slicing.py
@@ -0,0 +1,14 @@
+#INDEXING 
+str = "DANIYAL ARAIN"
+print(str[6])
+
+#SLICING
+str = "DANIYAL ARAIN"
+print(str[8:13])
+print(str[:4])
+print(str[0:])
+print(str[8:len(str)])
+
+#NEGATIVE INDEXING IN SLICING
+str1 = "DANIYAL"             # INDEX STARTS FROM -1 FROM RIGHT SIDE
+print(str1[-7:-3])
