aee28773d5a22b8df26f7f69139834afda67d1eb
Daniyal <daniyalarain123786@gmail.com>
2025-12-29 22:03:46 +0500

    Uncomment string concatenation and length example
    Uncommented code to demonstrate string concatenation and length.

---
+++ Chapter3-Strings/Concatenation_Length_Of_String.py
@@ -0,0 +1,6 @@
+#CONCATENATION AND LENGTH OF A STRING
+str1 = "apna"
+str2 = "college"
+final_str = str1 + " " + str2
+print(final_str)
+print("LENGTH :",len(final_str))    # "len" Function is used to display the length (No. of characters) of a string. 
