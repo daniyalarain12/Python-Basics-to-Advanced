02877e5206679562152dda0455df7b730b86b0f1
Daniyal <daniyalarain123786@gmail.com>
2025-12-29 21:53:32 +0500

    Uncomment string and escape sequence examples

---
+++ Chapter3-Strings/string_and_escape_sequences.py
@@ -0,0 +1,15 @@
+#STRINGS
+str1 = "I AM DANIYAL ARAIN."
+str2 = 'I AM DANIYAL ARAIN.'
+str3 = """I AM DANIYAL ARAIN."""
+str4 = '''I AM DANIYAL ARAIN.'''
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+
+#ESCAPE SEQUENCE CHARACTERS
+string1 = "I AM DANIYAL ARAIN.\nI AM A GOOD STUDENT."   # \n is used for newline. 
+string2 = "I AM DANIYAL ARAIN.\tI AM A GOOD STUDENT."   # \t is used for tab (8 spaces).   
+print(string1)
+print(string2)
