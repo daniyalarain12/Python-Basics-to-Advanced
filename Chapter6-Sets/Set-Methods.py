4547e371829ad2ad8b650aaa21482d67dd908094
Daniyal <daniyalarain123786@gmail.com>
2026-01-03 02:45:58 +0500

    Update Set-Methods.py

--- Chapter6-Sets/Set-Methods.py
+++ Chapter6-Sets/Set-Methods.py
@@ -2,21 +2,22 @@
 
 collection = set()                              # EMPTY SET
 
-collection.add(1)                               # ADDS AN ELEMENT
+collection.add(1)                                # ADDS AN ELEMENT
 collection.add(2)
 collection.add(2)
 collection.add(3)
 collection.add(4)
 print(collection)
 
-collection.remove(1)                            # REMOVES THE ELEMENT
+collection.remove(1)                             # REMOVES THE ELEMENT
 print(collection)
 
-print("DELETED ELEMENT :",collection.pop())     # REMOVES A RANDOM VALUE
+collection.clear()                               # EMPTIES THE SET
 print(collection)
 
-collection.clear()                              # EMPTIES THE SET
-print(collection)
+collection2 = {"DANIYAL","ARAIN",19}
+print("DELETED ELEMENT :",collection2.pop())     # REMOVES A RANDOM VALUE
+print(collection2)
 
 set1 = {1,2,3}
 set2 = {2,3,4}
