3d586a3895ddd59ca0f4a9af43f43130a1195bcf
Daniyal <daniyalarain123786@gmail.com>
2026-01-03 02:10:27 +0500

    Update Set-Basics.py

--- Chapter6-Sets/Set-Basics.py
+++ Chapter6-Sets/Set-Basics.py
@@ -8,13 +8,13 @@ set1 = {1,2,3,4}
 set2 = {1,2,3,1,3,4,2,5,4}
 set3 = {19,"DANIYAL",5.11,True,("OOP","DSA")}
 print(set1)
-print(set2)                      # DUPLICATE VALUES WILL NOT PRINT AND ONLY UNIQUE VALUES WILL PRINT
+print(set2)                      # DUPLICATE VALUES WILL ONLY PRINT ONCE
 print(set3)
 print(type(set1))
 print(type(set2))
 print(type(set3))
 print("LENGTH :",len(set1))
-print("LENGTH :",len(set2))      # ONLY UNIQUE VALUES WILL BE COUNTED
+print("LENGTH :",len(set2))      # DUPLICATE VALUES WILL ONLY COUNT ONCE
 print("LENGTH :",len(set3))
 
 empty_set = set()                # CREATING EMPTY SET
