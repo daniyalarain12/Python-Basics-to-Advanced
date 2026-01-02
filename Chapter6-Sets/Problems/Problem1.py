c0b2efee7f441edfc0795cb2c0561822cb36c7dc
Daniyal <daniyalarain123786@gmail.com>
2026-01-03 02:59:04 +0500

    Create Problem1.py

---
+++ Chapter6-Sets/Problems/Problem1.py
@@ -0,0 +1,9 @@
+# STORE FOLLOWING WORD MEANINGS IN A PYTHON DICTIONARY:
+# table : "a piece of furniture", "list of facts and figures"
+# "cat" : "a small animal"
+
+dict = {
+    "table" : ["a piece of furniture","list of facts and figures"],
+    "cat" : "a small animal"
+}
+print(dict)
