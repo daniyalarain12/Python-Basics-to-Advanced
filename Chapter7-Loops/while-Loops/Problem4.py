2d68cbc589607b24916ea05ff9abeec7c4fb9aad
Daniyal <daniyalarain123786@gmail.com>
2026-01-04 01:49:27 +0500

    Create Problem4.py

---
+++ Chapter7-Loops/while-Loops/Problem4.py
@@ -0,0 +1,7 @@
+# PRINT THE ELEMENTS OF THE FOLLOWING LIST USING A LOOP: [1,4,9,16,25,36,49,64,81,100]
+
+nums = [1,4,9,16,25,36,49,64,81,100]
+i = 0
+while i < len(nums):
+    print(nums[i])
+    i += 1
