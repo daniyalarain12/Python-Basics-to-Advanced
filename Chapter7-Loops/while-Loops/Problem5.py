bac3b52c6f25408c88ec0e94948f4f5fb193105c
Daniyal <daniyalarain123786@gmail.com>
2026-01-04 01:50:23 +0500

    Create Problem5.py

---
+++ Chapter7-Loops/while-Loops/Problem5.py
@@ -0,0 +1,9 @@
+# SEARCH FOR A NUMBER x IN THIS TUPLE USING LOOP: (1,4,9,16,25,36,49,64,81,100)
+
+nums = (1,4,9,16,25,36,49,64,81,100)
+x = int(input("ENTER THE TARGET : "))
+i = 0
+while i < len(nums):
+    if(nums[i]==x):
+        print("TARGET FOUND AT INDEX :",i)
+    i = i+1
