45ba356a502139e8270fe4c2d3783923f554c9d9
Daniyal <daniyalarain123786@gmail.com>
2026-01-04 21:32:14 +0500

    Update and rename Problem1.py to for-Loops-Basics.py

---
+++ Chapter7-Loops/for-Loops/for-Loops-Basics.py
@@ -0,0 +1,22 @@
+# FOR LOOPS
+# USED FOR SEQUENTIAL TRAVERSAL. FOR TRAVERSING LIST, STRING AND TUPLES etc.
+
+nums = [1,2,3,4,5]
+for val in nums:
+    print(val)
+
+fruits = ["APPLE","ORANGE","MANGO","BANANA"]
+for val in fruits:
+    print(val)
+
+tup = (1,2,3,4,2,8,9)
+for val in tup:
+    print(val)
+
+veg = ("POTATO","TOMATO","ONION","CARROT")
+for val in veg:
+    print(val)
+
+name = "DANIYAL ARAIN"
+for char in name:
+    print(char)
