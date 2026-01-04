31961e39e949eb3e611a821b2a89abb2cb2e658f
Daniyal <daniyalarain123786@gmail.com>
2026-01-04 23:42:58 +0500

    Create Problem9.py

---
+++ Chapter7-Loops/while-Loops/Problem9.py
@@ -0,0 +1,9 @@
+# WRITE A PROGRAM TO FIND THE SUM OF FIRST "n" NUMBER USING WHILE LOOP.
+
+n = int(input("ENTER ANY NUMBER : "))
+sum = 0
+i = 1
+while i<=n:
+    sum += i
+    i += 1
+print("SUM :",sum)
