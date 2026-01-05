476b4709fa75aed208853d523dfad6b8176f01a5
Daniyal <daniyalarain123786@gmail.com>
2026-01-05 14:37:27 +0500

    Create Problem6.py

---
+++ Chapter8-Functions/Problem6.py
@@ -0,0 +1,10 @@
+# WRITE A FUNCTION WHICH TAKES A NUMBER "n" AS INPUT FROM THE USER AND THEN CHECK IF A NUMBER IS EVEN OR ODD.
+
+def check():
+    n = int(input("ENTER ANY NUMBER : "))
+    if(n%2 == 0):
+        print("EVEN")
+    else:
+        print("ODD")
+
+check()  
