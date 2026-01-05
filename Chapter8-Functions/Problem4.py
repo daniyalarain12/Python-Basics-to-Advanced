ff9ff80634bb6f3677ae84cb199a2c26b7c49ea3
Daniyal <daniyalarain123786@gmail.com>
2026-01-05 14:21:09 +0500

    Create Problem4.py

---
+++ Chapter8-Functions/Problem4.py
@@ -0,0 +1,9 @@
+# WRITE A FUNCTION TO FIND THE FACTORIAL OF A NUMBER "n".
+
+def calc_fact(n):
+    fact = 1
+    for i in range(2,n+1):
+        fact *= i
+    print(n,"! =",fact)
+
+calc_fact(5)   
