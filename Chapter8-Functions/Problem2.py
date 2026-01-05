d54ea0d20e3e71fe627df656698bdc8670afc3f8
Daniyal <daniyalarain123786@gmail.com>
2026-01-05 14:06:32 +0500

    Update Problem2.py

--- Chapter8-Functions/Problem2.py
+++ Chapter8-Functions/Problem2.py
@@ -1,7 +1,7 @@
 # WRITE A FUNCTION TO PRINT THE LENGTH OF A LIST.
 
-def cal_len(list):
+def calc_len(list):
     print("LENGTH :",len(list))
 
 cities = ["LAHORE","HYDERABAD","KARACHI","ISLAMABAD","QUETTA","PESHAWAR"]
-cal_len(cities)
+calc_len(cities)
