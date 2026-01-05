0323add76bfa6fb1bef8122baf353fa81883cf75
Daniyal <daniyalarain123786@gmail.com>
2026-01-05 13:00:25 +0500

    Create Function-Types.py

---
+++ Chapter8-Functions/Function-Types.py
@@ -0,0 +1,11 @@
+# TYPES OF FUNCTIONS
+# 1. BUILT-IN FUNCTIONS
+# These functions are already provided by Python, so you don’t need to define them.
+# INCLUDES print, input, len, type and range FUNCTION.
+
+# PRINTING MULTIPLE print STATEMENTS IN A SINGLE LINE
+print("DANIYAL",end = " ")
+print("ARAIN")
+
+# 2. USER-DEFINED FUNCTIONS
+# These functions are created by the programmer using the def keyword to perform a specific task.
