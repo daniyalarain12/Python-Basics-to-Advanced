ec962c8df72ee0a5cff7e8d697e29f02cb8b2dfa
Daniyal <daniyalarain123786@gmail.com>
2026-01-06 22:52:05 +0500

    Create Writing-To-A-File.py

---
+++ Chapter10-File-IO/Writing-To-A-File.py
@@ -0,0 +1,7 @@
+# WRITING TO A FILE
+
+f = open("D:\Python\Chapter7\dani.txt","w")         # OVERWRITES THE ENTIRE FILE
+f.write("I AM DANIYAL ARAIN.")
+
+f = open("D:\Python\Chapter7\dani.txt","a")         # ADDS TO THE FILE
+f.write("\nI AM LEARNING PYTHON.")
