c8be252aeb7c84745da90512befe691247fc1851
Daniyal <daniyalarain123786@gmail.com>
2026-01-06 21:20:50 +0500

    Create Reading-A-File.py

---
+++ Chapter10-File-IO/Reading-A-File.py
@@ -0,0 +1,26 @@
+# READING A FILE
+
+# f.read()--> READS ENTIRE FILE.
+# f.readline()--> READS ONE LINE AT A TIME.
+
+f = open("D:\Python\Chapter7\dani.txt","r")
+data = f.read()
+data = f.read(10)                                  # READS ONLY 10 CHARACTERS
+print(data)
+f.close()
+
+f = open("D:\Python\Chapter7\demp.txt","r")
+line1 = f.readline()                               # READS 1ST LINE
+print(line1)
+line2 = f.readline()                               # READS 2ND LINE
+print(line2)            
+f.close()
+
+f = open("D:\Python\Chapter7\practice.txt","r")
+data = f.read()                                    # WHEN WE USE read(), BEFORE readline(), readline() WILL NOT READ ANY THING, BECAUSE ENTIRE FILE IS ALREADY READ BY read().
+print(data)
+line1 = f.readline()
+print(line1)
+line2 = f.readline()
+print(line2)
+f.close()
