01b4c870e0b4ad267cf429d67585c46a2d2e2cc6
Daniyal <daniyalarain123786@gmail.com>
2026-01-06 21:42:18 +0500

    Update Reading-A-File.py

--- Chapter10-File-IO/Reading-A-File.py
+++ Chapter10-File-IO/Reading-A-File.py
@@ -3,21 +3,22 @@
 # f.read()--> READS ENTIRE FILE.
 # f.readline()--> READS ONE LINE AT A TIME.
 
-f = open("D:\Python\Chapter7\dani.txt","r")
-data = f.read()
-data = f.read(10)                                  # READS ONLY 10 CHARACTERS
-print(data)
+f = open("D:\Python\Chapter7\demo.txt","r")
+data1 = f.read()
+data2 = f.read(10)                                # READS ONLY 10 CHARACTER
+print(data1)
+print(data2)
 f.close()
 
-# f = open("D:\Python\Chapter7\demo.txt","r")
-# data1 = f.read()
-# data2 = f.read(10)                                      # READS ONLY 10 CHARACTERS
-# print(data1)
-# print(data2)
-# f.close()
+f = open("D:\Python\Chapter7\dani.txt","r")
+line1 = f.readline()                              # READS 1ST LINE
+print(line1)
+line2 = f.readline()                              # READS 2ND LINE
+print(line2)
+f.close()
 
 f = open("D:\Python\Chapter7\practice.txt","r")
-data = f.read()                                    # WHEN WE USE read(), BEFORE readline(), readline() WILL NOT READ ANY THING, BECAUSE ENTIRE FILE IS ALREADY READ BY read().
+data = f.read()                                   # WHEN WE USE read() BEFORE readline(), readline() WILL NOT READ ANY THING, BECAUSE ENTIRE FILE IS ALREADY READ BY read().
 print(data)
 line1 = f.readline()
 print(line1)
