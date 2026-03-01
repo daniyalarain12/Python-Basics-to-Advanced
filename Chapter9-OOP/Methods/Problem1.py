fc8632615a1087c15c2464468c21c0181749efdb
Daniyal <daniyalarain123786@gmail.com>
2026-03-02 01:42:00 +0500

    Create Problem1.py

---
+++ Chapter9-OOP/Methods/Problem1.py
@@ -0,0 +1,18 @@
+# CREATE A STUDENT class THAT TAKE NAME AND MARKS OF 3 SUBJECTS AS AN ARGUMENTS IN CONSTRUCTOR. 
+# THEN CREATE A METHOD TO PRINT THE AVERAGE.
+
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def cal_avg(self):
+        sum = 0
+        for i in self.marks:
+            sum += i
+        print(self.name,"YOUR AVGEARGE IS :",sum/3)
+
+s1 = Student("DANIYAL",[99,98,97])
+s1.cal_avg()
+s1.name = "DANIYAL ARAIN"
+s1.cal_avg()
