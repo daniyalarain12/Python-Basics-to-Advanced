58a94b174c1bff67738f3f609572c900c344d87e
Daniyal <daniyalarain123786@gmail.com>
2026-03-02 17:12:43 +0500

    Create Private-Basics1.py

---
+++ Chapter9-OOP/Private-(Like)-Attributes-And-Methods/Private-Basics1.py
@@ -0,0 +1,23 @@
+# PRIVATE (LIKE) ATTRIBUTES AND METHODS
+# CONCEPTUAL IMPLEMENTATION IN PYTHON
+# PRIVATE ATTRIBUTES AND METHODS ARE MEANT TO BE USED ONLY WITHIN THE class AND ARE NOT ACCESSIBLE FROM OUTSIDE THE class.
+
+class Account:
+    def __init__(self, acc_no, acc_pass):
+        self.acc_no = acc_no
+        self.__acc_pass = acc_pass
+
+    def get_pass(self):
+        print(self.__acc_pass)
+
+acc1 = Account("24SW039","dani786")
+print(acc1.acc_no)
+
+# print(acc1.__acc_pass)              # CAN NOT ACCESS
+
+acc1.get_pass()
+print(acc1.acc_no)
+
+# print(acc1.__acc_pass)              # CAN NOT ACCESS
+
+acc1.get_pass()
