069ce27db45ca00fb1b46ca4b35e7ced2569bb6b
Daniyal <daniyalarain123786@gmail.com>
2026-03-04 22:28:38 +0500

    Create Multiple-Inheritance.py

---
+++ Chapter9-OOP/4-Pillars-Of-OOP/Inheritance/Multiple-Inheritance.py
@@ -0,0 +1,15 @@
+# MULTIPLE INHERITANCE ( PARENT --> CHILD <-- PARENT)
+
+class A:
+    varA = "WELCOME TO CLASS A"
+
+class B:
+    varB = "WELCOME TO CLASS B"
+
+class C(A, B):
+    varC = "WELCOME TO CLASS C"
+
+c1 = C()
+print(c1.varA)
+print(c1.varB)
+print(c1.varC)
