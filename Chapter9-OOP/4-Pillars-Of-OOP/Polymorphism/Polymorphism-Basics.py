9b15893dccc46885503c1a281953e6628ff3ec4f
Daniyal <daniyalarain123786@gmail.com>
2026-03-05 00:59:35 +0500

    Create Polymorphism-Basics.py

---
+++ Chapter9-OOP/4-Pillars-Of-OOP/Polymorphism/Polymorphism-Basics.py
@@ -0,0 +1,39 @@
+# POLYMORPHISM
+# OPERATOR OVERLOADING
+# WHEN THE SAME OPERATOR IS ALLOWED TO HAVE DIFFERENT MEANING ACCORDING TO THE CONTEXT.
+
+#OPERATORS AND DUNDER FUNCTIONS
+# a + b  ---> ADDITION         --->    a.__add__(b)
+# a - b  ---> SUBTRACTION      --->    a.__sub__(b)
+# a * b  ---> MULTIPLICATION   --->    a.__mul__(b)
+# a / b  ---> DIVISION         --->    a.__truediv__(b)
+# a % b  ---> MODULUS          --->    a.__mod__(b)
+
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+      
+    def showNumber(self):
+        print(self.real,"i +",self.img,"j")
+
+    def __add__(self, num):
+        newReal = self.real + num.real
+        newImg = self.img + num.img
+        return Complex(newReal, newImg)
+    def __sub__(self, num):
+        newReal = self.real - num.real
+        newImg = self.img - num.img
+        return Complex(newReal, newImg)
+
+num1 = Complex(1,3)
+num1.showNumber()
+
+num2 = Complex(4,6)
+num2.showNumber()
+
+sum = num1 + num2
+sum.showNumber()
+
+minus = num1 - num2
+minus.showNumber()
