9b1e7c06c2515fddf1fa2ccbd9b384f59102d2ca
Daniyal <daniyalarain123786@gmail.com>
2026-03-06 00:12:57 +0500

    Update Random-Password-Generator.py

--- Projects/Random-Password-Generator.py
+++ Projects/Random-Password-Generator.py
@@ -1,1 +1,12 @@
+# RANDOM PASSWORD GENERATOR
 
+import random, string
+
+pass_len = 12
+charValues = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+for i in range(pass_len):
+    password += random.choice(charValues)
+
+print("YOUR RANDOM PASSWORD IS :",password)
