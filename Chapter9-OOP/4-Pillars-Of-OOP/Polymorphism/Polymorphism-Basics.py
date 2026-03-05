5982be96d1d40f5b0420f0c17d0482a33343e6ee
Daniyal <daniyalarain123786@gmail.com>
2026-03-05 15:38:19 +0500

    Update Polymorphism-Basics.py

--- Chapter9-OOP/4-Pillars-Of-OOP/Polymorphism/Polymorphism-Basics.py
+++ Chapter9-OOP/4-Pillars-Of-OOP/Polymorphism/Polymorphism-Basics.py
@@ -13,27 +13,24 @@ class Complex:
     def __init__(self, real, img):
         self.real = real
         self.img = img
-      
     def showNumber(self):
         print(self.real,"i +",self.img,"j")
 
-    def __add__(self, num):
-        newReal = self.real + num.real
-        newImg = self.img + num.img
+    def __add__(self, num2):
+        newReal = self.real + num2.real
+        newImg = self.img + num2.img
         return Complex(newReal, newImg)
-    def __sub__(self, num):
-        newReal = self.real - num.real
-        newImg = self.img - num.img
+    def __sub__(self, num2):
+        newReal = self.real - num2.real
+        newImg = self.img - num2.img
         return Complex(newReal, newImg)
 
 num1 = Complex(1,3)
-num1.showNumber()
-
 num2 = Complex(4,6)
-num2.showNumber()
-
 sum = num1 + num2
-sum.showNumber()
-
 minus = num1 - num2
+
+num1.showNumber()
+num2.showNumber()
+sum.showNumber()
 minus.showNumber()
