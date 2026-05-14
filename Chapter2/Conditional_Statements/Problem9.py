314a8cd84cf3360144ae746a6cbe7eb8ef6d076b
Daniyal <daniyalarain123786@gmail.com>
2026-05-15 04:22:23 +0500

    Add ATM note counting program
    Implemented an ATM note counting program that calculates the number of 5000, 1000, and 500 notes based on user input. Added validation to ensure the input is a multiple of 500.

---
+++ Chapter2/Conditional_Statements/Problem9.py
@@ -0,0 +1,21 @@
+# Create an atm note counting program that takes a number as an input from the user and prints how many 5000, 1000 and 500 notes are there in the number.
+# - Example: 9500 is a number from the user which includes 1 note from 5000, 4 notes of 1000 and 1 note for 500.
+# - Note: add a restriction to the user to enter a  number that is a multiple of 500 only.
+
+num = int(input("ENTER ANY NUMBER (ONLY MULTIPLE OF 500) : "))
+note5000, note1000, note500 = 0, 0, 0
+
+if(num%500 == 0):
+    if(num >= 5000):
+        note5000 = num // 5000
+        num = num % 5000
+    if(num >= 1000):
+        note1000 = num // 1000
+        num = num % 1000
+    if(num >= 500):
+        note500 = num // 500
+    print("COUNT OF 5000 NOTES : ",note5000)
+    print("COUNT OF 1000 NOTES : ",note1000)
+    print("COUNT OF 500 NOTES : ",note500)
+else:
+    print("ENTER CORRECT NUMBER (MULTIPLE OF 500)")
