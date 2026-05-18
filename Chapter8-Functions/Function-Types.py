e52a37e41367c520556743cb0a847b021f652ddf
Daniyal <daniyalarain123786@gmail.com>
2026-05-19 03:35:12 +0500

    Update print statements and comments in Function-Types.py

--- Chapter8-Functions/Function-Types.py
+++ Chapter8-Functions/Function-Types.py
@@ -1,11 +1,15 @@
 # TYPES OF FUNCTIONS
+
 # 1. BUILT-IN FUNCTIONS
 # These functions are already provided by Python, so you don’t need to define them.
 # INCLUDES print, input, len, type and range FUNCTIONS.
 
-# PRINTING MULTIPLE print STATEMENTS IN A SINGLE LINE
-print("DANIYAL",end = " ")
+# END LINE PARAMETER
+print("DANIYAL",end = " ")           # PRINTING MULTIPLE print STATEMENTS IN A SINGLE LINE
 print("ARAIN")
+
+# SEPARATOR PARAMETER
+print("DANIYAL","ARAIN",sep="\n")    # PRINTING A SINGLE print STATEMENT IN MULTIPLE LINES
 
 # 2. USER-DEFINED FUNCTIONS
 # These functions are created by the programmer using the def keyword to perform a specific task.
