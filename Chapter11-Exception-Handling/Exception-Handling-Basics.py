d108ad72d92f4b79b5ac8025143775600940b306
Daniyal <daniyalarain123786@gmail.com>
2026-05-19 01:21:49 +0500

    Create Exception-Handling-Basics.py with example
    Add basic exception handling example in Python.

---
+++ Chapter11-Exception-Handling/Exception-Handling-Basics.py
@@ -0,0 +1,13 @@
+"""
+Exception Handling: Exception means a error.
+We had 2 clauses in Exception Handling:
+1. try: we provide the code to try if there is an error.
+2. except: except clause will only be executed when there is an error in try clause.
+"""
+
+list = [10,20,30,40,50]
+n = int(input("ENTER ANY NUMBER : "))
+try:
+    print(f"ELEMENT AT INDEX {n} IS :",list[n])
+except Exception as e:
+    print(e)
