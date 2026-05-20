208517dcb79fa111308158bd28fa809babfe20d5
Daniyal <daniyalarain123786@gmail.com>
2026-05-20 17:02:53 +0500

    Refactor file reading with relative paths and error handling
    Updated file paths to relative paths and added error handling for file operations.

--- Chapter10-File-IO/Reading-A-File.py
+++ Chapter10-File-IO/Reading-A-File.py
@@ -3,21 +3,21 @@
 # f.read()--> READS ENTIRE FILE.
 # f.readline()--> READS ONE LINE AT A TIME.
 
-f = open("D:\Python\Chapter7\demo.txt","r")
+f = open("demo.txt","r")
 data1 = f.read()
 data2 = f.read(10)                                # READS ONLY 10 CHARACTER
 print(data1)
 print(data2)
 f.close()
 
-f = open("D:\Python\Chapter7\dani.txt","r")
+f = open("dani.txt","r")
 line1 = f.readline()                              # READS 1ST LINE
 print(line1)
 line2 = f.readline()                              # READS 2ND LINE
 print(line2)
 f.close()
 
-f = open("D:\Python\Chapter7\practice.txt","r")
+f = open("practice.txt","r")
 data = f.read()                                   # WHEN WE USE read() BEFORE readline(), readline() WILL NOT READ ANY THING, BECAUSE ENTIRE FILE IS ALREADY READ BY read().
 print(data)
 line1 = f.readline()
@@ -25,3 +25,11 @@ print(line1)
 line2 = f.readline()
 print(line2)
 f.close()
+
+try:                                           # Using try-except to handle FileNotFoundError because an error occurs if the file does not exist.
+    f = open("daniyal.txt","r")
+    data = f.read()
+    print(data)
+    f.close()
+except FileNotFoundError as e:
+    print(e)
