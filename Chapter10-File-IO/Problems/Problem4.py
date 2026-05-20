953e01350884f504e3255baad21baaa5c949aaad
Daniyal <daniyalarain123786@gmail.com>
2026-05-20 17:47:03 +0500

    Add function to locate first line of 'LEARNING'
    Implement function to find first occurrence of 'LEARNING' in a file.

---
+++ Chapter10-File-IO/Problems/Problem4.py
@@ -0,0 +1,17 @@
+# WRITE A FUNCTION TO FIND IN WHICH LINE OF THE FILE DOES THE WORD "LEARNING" OCCUR FIRST. PRINT -1 IF NOT FOUND.
+
+def check():
+    word = "LEARNING"
+    count = 1
+    data = True
+    with open("practice.txt","r") as f:
+        while data:
+            data = f.readline()
+            if(word in data):
+                print(count)
+                return
+            count += 1
+
+    return -1
+
+print(check())
