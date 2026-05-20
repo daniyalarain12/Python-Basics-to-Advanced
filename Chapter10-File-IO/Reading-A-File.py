7e73b1bc7acbf5421e4a3e72ef24e86f7b6b939f
Daniyal <daniyalarain123786@gmail.com>
2026-05-20 17:31:23 +0500

    Enhance file reading with readlines and processing
    Added functionality to read all lines from a file and process them into lists.

--- Chapter10-File-IO/Reading-A-File.py
+++ Chapter10-File-IO/Reading-A-File.py
@@ -2,6 +2,7 @@
 
 # f.read()--> READS ENTIRE FILE.
 # f.readline()--> READS ONE LINE AT A TIME.
+# f.readlines() --> READS ALL LINES OF A FILE AND STORES EACH LINE AS A STRING IN A LIST.
 
 f = open("demo.txt","r")
 data1 = f.read()
@@ -34,7 +35,24 @@ line2 = f.readline()
 print(line2)
 f.close()
 
-f = open("D:\Python\Chapter8\data2.txt","r")
+f = open("data2.txt","r")
 line = f.readline()
 print(line.strip().split(","))                  # Remove extra spaces/newline, split data by comma, and convert it into a list.
+f.close()
+
+f = open("data2.txt","r")
+data = f.readlines()                        # READ ALL LINES FROM THE FILE AND STORE THEM IN A LIST.
+print(data)
+f.close()
+
+f = open("data2.txt","r")
+data = f.readlines()
+for line in data:
+    print(line)                             # READ ALL LINES AND PRINT EACH LINE ONE BY ONE, INCLUDING THE NEW LINE CHARACTER.
+f.close()
+
+f = open("data2.txt","r")
+data = f.readlines()
+for line in data:
+    print(line.strip().split(","))          # READ ALL LINES, REMOVE EXTRA SPACES/NEWLINES, SPLIT DATA BY COMMA, AND CONVERT EACH LINE INTO A LIST.
 f.close()
