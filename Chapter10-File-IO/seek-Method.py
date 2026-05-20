599c8dcce8258d51c11df8f394f85eb1fba9dfbe
Daniyal <daniyalarain123786@gmail.com>
2026-05-20 21:13:08 +0500

    Change file paths to relative in seek-Method.py
    Updated file paths to use relative paths instead of absolute paths.

--- Chapter10-File-IO/seek-Method.py
+++ Chapter10-File-IO/seek-Method.py
@@ -2,21 +2,21 @@
 # Used to move the file cursor to a specific position.
 
 # r+ MODE
-with open("D:\Python\Chapter8\data4.txt","r+") as f:
+with open("data4.txt","r+") as f:
     data = f.read()
     end = len(data)
     f.seek(end)            # Move cursor to the end of the file
     f.write("DANIYAL")     # Add new data at the end of the file
 
 # w+ MODE
-with open("D:\Python\Chapter8\data4.txt","r+") as f:
+with open("data4.txt","r+") as f:
     f.write("DANIYAL")
     f.seek(0)                # Move cursor back to the start for reading
     data = f.read()
     end = len(data)
 
 # a+ MODE
-with open("D:\Python\Chapter8\data4.txt","a+") as f:
+with open("data4.txt","a+") as f:
     f.seek(0)                # Move cursor to the start for reading data
     data = f.read()
     end = len(data)
