1be1c6bc2069655d157efb36049c8e2eec84feb1
Daniyal <daniyalarain123786@gmail.com>
2026-05-20 19:44:50 +0500

    Implement writing list data to a CSV file
    Added functionality to write a list of strings to a CSV file.

--- Chapter10-File-IO/Writing-To-A-File.py
+++ Chapter10-File-IO/Writing-To-A-File.py
@@ -7,3 +7,11 @@ f = open("dani.txt","a")         # ADDS TO THE FILE
 f.write("\nI AM LEARNING PYTHON.")
 
 f.close()
+
+# f.writelines() ---> Writes a list of strings into a file (does not add new line automatically).
+
+listData = ["DANIYAL,GHULAM SHABBIR,03092313464,daniyalarain123786@gmail.com,da12\n",
+            "SAMI,GHULAM SAGHEER,03492313464,samiarain123786@gmail.com,sa29"]
+
+with open("data.csv","w") as f:
+    f.writelines(listData)
