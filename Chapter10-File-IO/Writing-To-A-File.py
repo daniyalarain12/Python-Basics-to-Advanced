5660ea84fd91bcdf154318ec0e3c9d9b6a75ef61
Daniyal <daniyalarain123786@gmail.com>
2026-05-20 18:02:39 +0500

    Update file path to relative in Writing-To-A-File.py

--- Chapter10-File-IO/Writing-To-A-File.py
+++ Chapter10-File-IO/Writing-To-A-File.py
@@ -1,9 +1,9 @@
 # WRITING TO A FILE
 
-f = open("D:\Python\Chapter7\dani.txt","w")         # OVERWRITES THE ENTIRE FILE
+f = open("dani.txt","w")         # OVERWRITES THE ENTIRE FILE
 f.write("I AM DANIYAL ARAIN.")
 
-f = open("D:\Python\Chapter7\dani.txt","a")         # ADDS TO THE FILE
+f = open("dani.txt","a")         # ADDS TO THE FILE
 f.write("\nI AM LEARNING PYTHON.")
 
 f.close()
