a4a11d700e34e78c1abb84f7c0d91055db325e6a
Daniyal <daniyalarain123786@gmail.com>
2026-05-21 23:02:03 +0500

    Update Math-Module-Basics.py

--- Chapter12-Modules/Math-Module/Math-Module-Basics.py
+++ Chapter12-Modules/Math-Module/Math-Module-Basics.py
@@ -1,27 +1,32 @@
-# MATH MODULE
+# MODULES
+# A module in Python is a file that contains reusable code such as functions, attributes and classes.
 
-import math
+# math MODULE
+# It is built-in Python module that provides mathematical functions and constants.
 
-print(math.pow(2,3))
+import math                      # Importing Python built-in math module
 
-print(math.sqrt(25))
-print(math.isqrt(25))
-print(math.isqrt(30))
+print(math.pow(2,3))             # Returns power of a number (2^3)
 
-print(math.cbrt(125))
+print(math.sqrt(25))             # Returns square root of 25
+print(math.isqrt(25))            # Returns integer square root of 25
+print(math.isqrt(30))            # Returns integer square root of 30
 
-print(math.factorial(5))
+print(math.cbrt(125))            # Returns cube root of 125
 
+print(math.factorial(5))         # Returns factorial of 5
+
+# Trigonometric functions
 print(math.sin(0))
 print(math.cos(0))
 print(math.tan(0))
 
-print(math.log10(100))
+print(math.log10(100))           # Returns base-10 logarithm of 100
 
-print(math.floor(3.67))
-print(math.ceil(3.67))
+print(math.floor(3.67))          # Rounds number down to nearest integer
+print(math.ceil(3.67))           # Rounds number up to nearest integer
 
-print(math.prod([2,3,6,2]))
+print(math.prod([2,3,6,2]))      # Returns product of all elements in list
 
-print(math.pi)
-print(math.nan)
+print(math.pi)                   # Mathematical constant PI
+print(math.nan)                  # Represents Not a Number (NaN)
