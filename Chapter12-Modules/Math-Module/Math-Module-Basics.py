239b4971a1760982e6e3a5383e0a68ddb6297e1d
Daniyal <daniyalarain123786@gmail.com>
2026-05-21 22:48:33 +0500

    Add Math Module Basics with various functions
    Implement basic math operations using the math module.

---
+++ Chapter12-Modules/Math-Module/Math-Module-Basics.py
@@ -0,0 +1,27 @@
+# MATH MODULE
+
+import math
+
+print(math.pow(2,3))
+
+print(math.sqrt(25))
+print(math.isqrt(25))
+print(math.isqrt(30))
+
+print(math.cbrt(125))
+
+print(math.factorial(5))
+
+print(math.sin(0))
+print(math.cos(0))
+print(math.tan(0))
+
+print(math.log10(100))
+
+print(math.floor(3.67))
+print(math.ceil(3.67))
+
+print(math.prod([2,3,6,2]))
+
+print(math.pi)
+print(math.nan)
