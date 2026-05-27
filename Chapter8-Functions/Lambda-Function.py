4b966fc59b3a7ba2515ba6de70ab75e5fc2634a9
Daniyal <daniyalarain123786@gmail.com>
2026-05-28 02:47:45 +0500

    Create Lambda-Function.py

---
+++ Chapter8-Functions/Lambda-Function.py
@@ -0,0 +1,12 @@
+# LAMBDA FUNCTION
+# A lambda function in Python is a small anonymous function created using the lambda keyword. It can take any number of arguments but contains
+# only one expression, whose result is returned automatically. Lambda functions are commonly used for short, temporary operations and inline logic.
+
+# SYNTAX
+# function_name = lambda arguments : expression
+
+sum = lambda a,b : a+b
+avg = lambda a,b : (a+b)/2
+
+print("SUM :",sum(2,4))
+print("AVERAGE :",avg(2,4))
