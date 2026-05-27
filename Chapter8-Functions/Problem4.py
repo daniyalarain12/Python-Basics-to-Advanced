bdca8fc0613e0b25f00f3b29903120b4b4abe8c7
Daniyal <daniyalarain123786@gmail.com>
2026-05-28 02:49:49 +0500

    Delete Chapter8-Functions/Problem4.py

--- Chapter8-Functions/Problem4.py
+++
@@ -1,9 +0,0 @@
-# WRITE A FUNCTION TO FIND THE FACTORIAL OF A NUMBER "n".
-
-def calc_fact(n):
-    fact = 1
-    for i in range(2,n+1):
-        fact *= i
-    print(n,"! =",fact)
-
-calc_fact(5)   
