a4259e140e644e0d7f05f4894a6eb0a52b980891
Daniyal <daniyalarain123786@gmail.com>
2026-05-29 01:03:24 +0500

    Create List-Comprehension-Basics.py

---
+++ Chapter4-Lists-and-Tuples/Lists/List-Comprehension/List-Comprehension-Basics.py
@@ -0,0 +1,11 @@
+# LIST COMPREHENSION
+# List comprehension in Python is a short and simple way to create a new list from an existing iterable using a single line of code.
+
+# SYNTAX
+# new_list = [expression for item in iterable if condition]
+# new_list = [value_if_true if condition else value_if_false for item in iterable]
+
+# expression -> Value or operation to add in the new list
+# item -> Variable used to access each element
+# iterable -> Collection like list, tuple, set, etc.
+# condition -> Optional filter to check whether item should be added
